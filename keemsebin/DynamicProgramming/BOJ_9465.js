448ebe30d86fb576f37a94bb0068254f1911facd
keemsebeen <sebin0580@gmail.com>
2024-04-27 00:26:46 +0900

    chore: 오타 수정

--- keemsebin/DynamicProgramming/BOJ_9465.js
+++ keemsebin/DynamicProgramming/BOJ_9465.js
@@ -8,7 +8,7 @@ for (let i = 0; i < T; i++) {
   const score2 = input[i * 3 + 3].split(" ").map(Number);
 
   const dp = [[0], [...score1], [...score2]];
-  ㄴ;
+
   for (let j = 1; j < case1; j++) {
     dp[0][j] = Math.max(dp[1][j - 1], dp[2][j - 1]); // 아무것도 선택하지 않았을 경우 직전 열 중 최댓값
     dp[1][j] = Math.max(dp[0][j - 1], dp[2][j - 1]) + score1[j];
