c95323435c86fa026d0ce15deb853699d4901e62
nakyong82 <nakyong7770@gmail.com>
2024-05-20 23:31:55 +0900

    ✨feat: BOJ_2109_순회강연 풀이 완료

--- naarang/Greedy/BOJ_2109.js
+++ naarang/Greedy/BOJ_2109.js
@@ -5,6 +5,7 @@ let input = fs
   .trim()
   .split("\n");
 
+// 비용이 큰 순서로 정렬하되, 그 비용에 해당하는 날짜를 역순으로 1일까지 탐색하면서 들어갈 자리가 있는지 확인
 let N = Number(input.shift());
 let money = [];
 let answer = 0;
@@ -14,34 +15,18 @@ for (let i = 0; i < N; i++) {
 }
 money.sort((a, b) => b[0] - a[0]);
 
-while (money.length > 0) {
-  let select = 0; // 그날 가기로 한 index
-  for (let i = 0; i < money.length; i++) {
-    if (select + 1 >= money[i][1]) {
-      // 해당 부분 강연가기
+// 여기서 날짜를 채워가기
+let day = new Array(1001).fill(false);
+
+for (let m of money) {
+  while (m[1] > 0) {
+    if (!day[m[1]]) {
+      day[m[1]] = true;
+      answer += m[0];
       break;
     }
-    select++;
-  }
-
-  if (select >= money.length) {
-    select = 0; // 만약에 급한게 없으면 첫번째 강의 선택
-  }
-
-  answer += money[select][0];
-  money.splice(select, 1); // 간 곳 삭제
-
-  // 다음날되면 감소
-  for (let i = 0; i < money.length; i++) {
-    money[i][1] -= 1;
-    if (money[i][1] <= 0) {
-      // 이제 못가는 곳 삭제
-      money.splice(i, 1);
-      continue;
-    }
+    m[1] -= 1;
   }
 }
 
 console.log(answer);
-
-// 배열.splice(시작 인덱스, 삭제할 원소 개수) 함수 : 배열의 특정 인덱스 제거 -> 우선 순위 큐 이용해보기
