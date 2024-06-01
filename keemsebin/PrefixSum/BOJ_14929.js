714617b7b6362b5cac51692a276cb5609129e6ad
keemsebeen <sebin0580@gmail.com>
2024-06-02 01:17:51 +0900

    comment : 14929 주석 추가

--- keemsebin/PrefixSum/BOJ_14929.js
+++ keemsebin/PrefixSum/BOJ_14929.js
@@ -8,9 +8,10 @@ let sum = 0;
 const total = arr.reduce((acc, cur) => acc + cur, 0);
 
 // 부분쌍의 곱의 합
+// 나 자신 * 나 자신을 제외한 나머지
 for (let i = 0; i < N; i++) {
   sum += arr[i] * (total - arr[i]); // 1 * 2-1 + -2 * 2+1 + 3 * 2-2
 }
-
+// 2번씩 계산돼서 나눠줘야됨
 sum = sum / 2;
 console.log(sum);
